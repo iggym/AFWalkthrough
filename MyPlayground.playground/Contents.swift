e1069a4f4f95a0a73b42f32dee2b113088f3d76b
iggym <iggy.mw@gmail.com>
2017-03-20 22:19:37 -0500

    created project, pod file and workspace, added playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import PlaygroundSupport
+import Alamofire
+
+
+//var str = "Hello, playground"
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+URLCache.shared = URLCache(memoryCapacity: 0, diskCapacity: 0, diskPath: nil)
+
+Alamofire.request("https://httpbin.org/get")
+
+Alamofire.request("https://httpbin.org/get").responseJSON { response in
+    print(response.request)  // original URL request
+    print(response.response) // HTTP URL response
+    print(response.data)     // server data
+    print(response.result)   // result of response serialization
+    
+    if let JSON = response.result.value {
+        print("JSON: \(JSON)")
+    }
+}
