3c433ab969a1dfc9db2ae8428bf83040c74da2f8
iggym <iggy.mw@gmail.com>
2017-03-22 22:52:34 -0500

    pod file added

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import PlaygroundSupport
+import Alamofire
+
+
+var str = "Hello, playground"
+
+//let testUrl = "https://httpbin.org/get"
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+URLCache.shared = URLCache(memoryCapacity: 0, diskCapacity: 0, diskPath: nil)
+
+Alamofire.request(testUrl).responseJSON { response in
+    print(response.request)  // original URL request
+    print(response.response) // HTTP URL response
+    print(response.data)     // server data
+    print(response.result)   // result of response serialization
+    
+    if let JSON = response.result.value {
+        print("JSON: \(JSON)")
+    }
+    print(" ****************\n\n\n ")
+}
---
+++ Podfile
@@ -0,0 +1,20 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+
+target 'AFWalkthrough' do
+  # Pods for AFWalkthrough
+   pod 'Alamofire', '~> 4.4'
+   pod 'SwiftyJSON'
+
+ target 'AFWalkthroughTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'AFWalkthroughUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
