dd0d1c146b4c51b84a1bf507ce75ea12c4f07ec8
iggym <iggy.mw@gmail.com>
2017-03-25 21:01:38 -0500

    added more examples

--- Examples.playground/Contents.swift
+++ Examples.playground/Contents.swift
@@ -1,5 +1,16 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+import SwiftyJSON
+import Foundation
 
-var str = "Hello, playground"
+var jsonData: Data?
+
+if let file = Bundle.main.path(forResource: "SwiftyJSONTests", ofType: "json") {
+    jsonData = try? Data(contentsOf: URL(fileURLWithPath: file))
+} else {
+    print("Fail")
+}
+
+let jsonObject = try JSONSerialization.jsonObject(with: jsonData!, options: .allowFragments) as? [[String: AnyObject]]
+
+let jsonString = String(data: jsonData!, encoding: .utf8)
